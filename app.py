2ca9af5ed23f61711e2ab3a7020bb731d56cbc86
Krishna Teja Reddy <vkrishr.teja@gmail.com>
2017-02-26 11:00:49 +0530

    Added Edit and Delete functionalities

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def school_reg():
 		if len(exists.fetchall()):
 			error = 'School already exists'
 		else:
-			g.db.execute('insert into school values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['sname'],request.form['board'],request.form['type'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pwd'],request.form['website'],request.form['c1name'],request.form['c1designation'],request.form['c1phone'],request.form['c1email'],request.form['c2name'],request.form['c2designation'],request.form['c2phone'],request.form['c2email']])
+			g.db.execute('insert into school values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['sname'],request.form['board'],request.form['type'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pwd'],request.form['website'],request.form['c1name'],request.form['c1designation'],request.form['c1phone'],request.form['c1email'],request.form['c2name'],request.form['c2designation'],request.form['c2phone'],request.form['c2email'],None])
 			g.db.commit()
 			return redirect(url_for('index'))
 	return error
@@ -136,7 +136,7 @@ def student_reg():
 		if len(exists.fetchall()):
 			error = 'Student already exists'
 		else:
-			g.db.execute('insert into student values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['gender'],request.form['class'],request.form['section'],request.form['rollno'],request.form['sname'],request.form['pname'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['percent'],request.form['pic']])
+			g.db.execute('insert into student values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['gender'],request.form['class'],request.form['section'],request.form['rollno'],request.form['sname'],request.form['pname'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['percent'],request.form['pic'],None])
 			g.db.commit()
 			return redirect(url_for('index'))
 	return error
@@ -152,7 +152,7 @@ def business_reg():
 		if len(exists.fetchall()):
 			error = 'Business Partner already exists'
 		else:
-			g.db.execute('insert into business values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['occupation'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pan'],request.form['resume'],request.form['location']])
+			g.db.execute('insert into business values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['occupation'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pan'],request.form['resume'],request.form['location'],None])
 			g.db.commit()
 			return redirect(url_for('index'))
 	return error
@@ -176,7 +176,7 @@ def volunteer_reg():
 			fund = 'fund' in request.form
 			projects = 'projects' in request.form
 			sporting = 'sporting' in request.form
-			g.db.execute('insert into volunteer values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['skill'],request.form['marital'],mentor,tutor,teaching,counselling,organising,fund,projects,sporting,request.form['days'],request.form['time']])
+			g.db.execute('insert into volunteer values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['skill'],request.form['marital'],mentor,tutor,teaching,counselling,organising,fund,projects,sporting,request.form['days'],request.form['time'],None])
 			g.db.commit()
 			return redirect(url_for('index'))
 	return error
@@ -192,10 +192,94 @@ def intern_reg():
 		if len(exists.fetchall()):
 			error = 'Intern already exists'
 		else:
-			g.db.execute('insert into internship values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['language'],request.form['university'],request.form['courses'],request.form['skills'],request.form['months'],request.form['from'],request.form['to'],request.form['interest'],request.form['location'],request.form['motivation']])
+			g.db.execute('insert into internship values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['language'],request.form['university'],request.form['courses'],request.form['skills'],request.form['months'],request.form['from'],request.form['to'],request.form['interest'],request.form['location'],request.form['motivation'],None])
 			g.db.commit()
 			return redirect(url_for('index'))
 	return error
+	
+
+@app.route("/school_update", methods = ['GET', 'POST'])
+def school_update():
+	if request.method == 'POST':
+		g.db.execute('update school set SchoolName=?, Board=?, Type=?, PhoneNumber=?, Address=?, City=?, State=?, Zip=?, Email=?, Password=?, Website=?, c1Name=?, c1Designation=?, c1Phone=?, c1Email=?, c2Name=?, c2Designation=?, c2Phone=?, c2Email=? where Id=?',[request.form['sname'],request.form['board'],request.form['type'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pwd'],request.form['website'],request.form['c1name'],request.form['c1designation'],request.form['c1phone'],request.form['c1email'],request.form['c2name'],request.form['c2designation'],request.form['c2phone'],request.form['c2email'],request.form['id']])
+		g.db.commit()
+		return redirect(url_for('loggedin'))
+	
+
+@app.route("/student_update", methods = ['GET', 'POST'])
+def student_update():
+	if request.method == 'POST':
+		g.db.execute('update student set FirstName=?, LastName=?, Dob=?, Gender=?, Class=?, Section=?, RollNo=?, SchoolName=?, PName=?, PhoneNumber=?, Address=?, City=?, State=?, Zip=?, Email=?, Percentage=?, Photo=? where Id=?',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['gender'],request.form['class'],request.form['section'],request.form['rollno'],request.form['sname'],request.form['pname'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['percent'],request.form['pic'],request.form['id']])
+		g.db.commit()
+		return redirect(url_for('loggedin'))
+	
+
+@app.route("/volunteer_update", methods = ['GET', 'POST'])
+def volunteer_update():
+	if request.method == 'POST':
+		mentor = 'mentor' in request.form
+		tutor = 'tutor' in request.form
+		teaching = 'teaching' in request.form
+		counselling = 'counselling' in request.form
+		organising = 'organising' in request.form
+		fund = 'fund' in request.form
+		projects = 'projects' in request.form
+		sporting = 'sporting' in request.form
+		g.db.execute('update volunteer set FirstName=?, LastName=?, Dob=?, PhoneNumber=?, Address=?, City=?, State=?, Zip=?, Email=?, Skills=?, MaritalStatus=?, Mentoring=?, Tutoring=?, Teaching=?, Counselling=?, CompetitionActivities=?, FundRaising=?, ProjectsEvents=?, Sporting=?, PreferredDays=?, PreferredTiming=? where Id=?',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['skill'],request.form['marital'],mentor,tutor,teaching,counselling,organising,fund,projects,sporting,request.form['days'],request.form['time'],request.form['id']])
+		g.db.commit()
+		return redirect(url_for('loggedin'))
+	
+
+@app.route("/intern_update", methods = ['GET', 'POST'])
+def intern_update():
+	if request.method == 'POST':
+		g.db.execute('update internship set FirstName=?, LastName=?, Dob=?, Picture=?, Gender=?, PhoneNumber=?, Address=?, City=?, State=?, Zip=?, Email=?, Languages=?, University=?, AdditionalCourses=?, SpecificSkills=?, Months=?, FromDate=?, ToDate=?, Interest=?, Location=?, Motivation=? where Id=?',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['language'],request.form['university'],request.form['courses'],request.form['skills'],request.form['months'],request.form['from'],request.form['to'],request.form['interest'],request.form['location'],request.form['motivation'],request.form['id']])
+		g.db.commit()
+		return redirect(url_for('loggedin'))
+	
+
+@app.route("/business_update", methods = ['GET', 'POST'])
+def business_update():
+	if request.method == 'POST':
+		g.db.execute('update business set FirstName=?, LastName=?, Dob=?, Occupation=?, Picture=?, Gender=?, PhoneNumber=?, Address=?, City=?, State=?, Zip=?, Email=?, PAN=?, Resume=?, Location=? where Id=?',[request.form['fname'],request.form['lname'],request.form['dob'],request.form['occupation'],request.form['pic'],request.form['gender'],request.form['phone'],request.form['address'],request.form['city'],request.form['state'],request.form['zip'],request.form['email'],request.form['pan'],request.form['resume'],request.form['location'],request.form['id']])
+		g.db.commit()
+		return redirect(url_for('loggedin'))
+	
+	
+	
+@app.route("/remove_school", methods = ['GET', 'POST'])
+def remove_school():
+	g.db.execute('delete from school where Id = ?',[request.form['remove']])
+	g.db.commit()
+	return redirect(url_for('loggedin'))
+	
+	
+@app.route("/remove_student", methods = ['GET', 'POST'])
+def remove_student():
+	g.db.execute('delete from student where Id = ?',[request.form['remove']])
+	g.db.commit()
+	return redirect(url_for('loggedin'))
+	
+	
+@app.route("/remove_volunteer", methods = ['GET', 'POST'])
+def remove_volunteer():
+	g.db.execute('delete from volunteer where Id = ?',[request.form['remove']])
+	g.db.commit()
+	return redirect(url_for('loggedin'))
+	
+	
+@app.route("/remove_intern", methods = ['GET', 'POST'])
+def remove_intern():
+	g.db.execute('delete from internship where Id = ?',[request.form['remove']])
+	g.db.commit()
+	return redirect(url_for('loggedin'))
+	
+	
+@app.route("/remove_business", methods = ['GET', 'POST'])
+def remove_business():
+	g.db.execute('delete from business where Id = ?',[request.form['remove']])
+	g.db.commit()
+	return redirect(url_for('loggedin'))
 
 	
 
